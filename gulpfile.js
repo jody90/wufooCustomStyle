e259a4d851c95068cba1bbbe4a5711ed1e1b08d2
Jody <jlerch@sortimo.de>
2017-02-24 08:55:26 +0100

    Gulp Prozesse optimiert.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,12 @@
-var gulp  = require('gulp');
-var watch = require('gulp-watch');
-var sass  = require('gulp-sass');
+var gulp       = require('gulp');
+var watch      = require('gulp-watch');
+var sass       = require('gulp-sass');
+var uglify     = require('gulp-uglify');
+var concat     = require('gulp-concat');
+var sourcemaps = require('gulp-sourcemaps');
+var cleanCss   = require('gulp-clean-css');
+var rename     = require('gulp-rename');
+var livereload = require('gulp-livereload');
 
 function handleError(err) {
     console.error(err.toString());
@@ -12,9 +18,20 @@ gulp.task('watch', function() {
     gulp.watch(['./sass/*.scss'], ['sass']);
 })
 
+gulp.task('js', function() {
+    console.log("js");
+})
+
 gulp.task('sass', function () {
-  return gulp.src('./sass/*.scss')
+    console.log("sass");
+    return gulp.src('./sass/style.scss')
+    .pipe(sourcemaps.init())
     .pipe(sass().on('error', handleError))
+    .pipe(cleanCss())
+    .pipe(sourcemaps.write())
+    .pipe(rename({
+        basename: "wufoo"
+    }))
     .pipe(gulp.dest('./css'));
 });
 
